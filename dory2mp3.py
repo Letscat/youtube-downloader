e5d607e6afaeb38fd2dff3b1c579232cf557f873
Letscat <43135049+Letscat@users.noreply.github.com>
2022-05-21 00:42:10 +0200

    macos v1.2

--- dory2mp3.py
+++ dory2mp3.py
@@ -82,22 +82,22 @@ def dory2mp3():
     
 
 
-    playlist=Entry(tab1,width=50)
-    playlist.place(rely=0.5, relx=0.5, x=0, y=-10, anchor=CENTER)
+    playlist=Entry(tab1,width=40)
+    playlist.place(rely=0.5, relx=0.5, x=0, y=0, anchor=CENTER)
     lbl_progress = Label(tab1,text= str(downloaded_songs) +"von"+ str(amount_of_songs))
-    lbl_progress.place(rely=1, relx=0, x=0, y=0, anchor=SW)
+    lbl_progress.place(rely=0, relx=1, x=0, y=0, anchor=NE)
 
 
     btn1 = Button(tab1, text= 'Download Playlist', command=setplaylist)
 
     btn1.place(rely=1.0, relx=1.0, x=0, y=0, anchor=SE)
 
-    btn1 = Button(tab1, text= 'Download 1 Song', command=setsong)
+    btn2 = Button(tab1, text= 'Download 1 Song', command=setsong)
 
-    btn1.place(rely=1.0, relx=0.75, x=0, y=0, anchor=SE)
+    btn2.place(rely=1.0, relx=0, x=0, y=0, anchor=SW)
 
     lbl_j = Label(tab2,text= "Created for my beloved dory")
-    lbl_j.place(rely=0.4, relx=0.6, x=0, y=0, anchor=CENTER)
+    lbl_j.place(rely=0.5, relx=0.6, x=0, y=0, anchor=CENTER)
 
 
     lbl_c = Label(tab2,text="©2022 Michael Schmid" )
@@ -118,7 +118,7 @@ def dory2mp3():
     canvas.create_image(0, 0, anchor = NW, image=photo)
     canvas.place(relx=0.0, rely=1.0, anchor=SW)
 
-    window.geometry('450x110')
+    window.geometry('450x160')
     window.resizable(False, False)
     window.mainloop()
     try:
