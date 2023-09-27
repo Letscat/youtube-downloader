b788fe9ebbae38c6a0279c46ed7dc2d1d6792ac1
Michael Schmid <michael15schmid@gmail.com>
2023-09-27 21:38:26 +0200

    new ui

--- dory2mp3.py
+++ dory2mp3.py
@@ -1,41 +1,33 @@
-from tkinter import ttk
-from moviepy.audio.io.AudioFileClip import AudioFileClip
-from tkinter import *
-from mutagen.easyid3 import EasyID3
+import customtkinter
 from pytube import YouTube,Playlist
 from pytube.cli import on_progress
 from proglog import ProgressBarLogger
-import os
-import string
+from mutagen.easyid3 import EasyID3
+from moviepy.audio.io.AudioFileClip import AudioFileClip
 import threading
+import string
+import os
 
-
-
-fDownloadSize=0
+fDownload=0
 fDownloaded=0
-
-fConvertSize=0
+sDestination = os.path.join(os.path.expanduser("~"), "Music")
+fConvert=0
 fConverted=0
 
-destination = os.path.join(os.path.expanduser("~"), "Music")
-
-
-def dory2mp3():
-    def setplaylist():
-        oList = Playlist(playlist.get())
+def prepare_content(oInput):
+    if "list=" in oInput:
+        oList=Playlist(oInput)
         for oContent in oList.videos:
             threading.Thread(target=downloadStream,
-                             args=(oContent, destination, oList.length)).start()
-
-            # prints address of each YouTube object in the playlist
-    def setsong():
-        url = playlist.get()
-        oVideo=YouTube(url)
+                             args=(oContent, sDestination, oList.length)).start()
+            
+    else:
+        oVideo=YouTube(oInput)
         threading.Thread(target=downloadStream,
-                         args=(oVideo, destination, 1)).start()
-
+                         args=(oVideo, sDestination, 1)).start()
+        
     def downloadStream(oContent, destination, iPlaylistLength):
-        global fDownloadSize
+        global fDownload
         
         if oContent.age_restricted:
             print("Age restricted")
@@ -48,7 +40,7 @@ def dory2mp3():
         except:
             print("Stream couldn't be accessed")
             return
-        fDownloadSize+=round(oStream.filesize_mb,1)
+        fDownload+=round(oStream.filesize_mb,1)
         oContent.register_on_progress_callback(my_progress_function)
         sFileName=destination +"/"+ remove_invalid_chars(oContent.title) + ".mp3"
         print("started Downloading Video:"+oContent.title )
@@ -78,7 +70,7 @@ def dory2mp3():
         return ''.join(c for c in filename if c in valid_chars)
     def my_progress_function(stream, chunk, bytes_remaining):
         global fDownloaded
-        global fDownloadSize
+        global fDownload
         
         if bytes_remaining > 9437184:
             fDownloaded+=9
@@ -86,8 +78,8 @@ def dory2mp3():
             fDownloaded+=round(len(chunk)/1048576,1)
 
         #progress = round(bytes_downloaded / total_size * 100)
-        pb['value'] = 100*fDownloaded/fDownloadSize
-        lbl_Progress['text']=fDownloaded,"/",fDownloadSize," MB"
+        pb['value'] = 100*fDownloaded/fDownload
+        lbl_Progress['text']=fDownloaded,"/",fDownload," MB"
     class MyBarLogger (ProgressBarLogger):
         def bars_callback (self, bar, attr, value,old_value=0):
             global fConvertSize
@@ -97,68 +89,28 @@ def dory2mp3():
                 return 
             fConverted+=value-old_value
             pb1['value'] = 100*fConverted/fConvertSize
-            percentage = (value / self.bars [bar] ['total']) * 100
     logger = MyBarLogger ()
-    window = Tk()
-    window.title("Dory2mp3")
-    window.iconbitmap("dory.ico")
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
 
-    tab_control = ttk.Notebook(window)
+        self.title("Dory2Mp3")
+        self.geometry("500x250")
+        self.grid_columnconfigure((0, 1), weight=1)
+        self.entry = customtkinter.CTkEntry(self, placeholder_text="Video / Playlist")
+        self.entry.grid(row=0, column=0, padx=20, pady=20, sticky="ew", columnspan=2)
+        self.button = customtkinter.CTkButton(self, text="my button", command=self.button_callback)
+        self.button.grid(row=1, column=0, padx=20, pady=20, sticky="ew", columnspan=2)
 
-    tab1 = ttk.Frame(tab_control)
+    def button_callback(self):
+        print("button pressed")
+        oInput=self.entry.get()
+        if oInput=="":
+            print("No Input Entered")
+            return
+        prepare_content(oInput)
 
-    tab2 = ttk.Frame(tab_control)
+app = App()
+app.mainloop()
 
-    tab_control.add(tab1, text='Download')
-
-    tab_control.add(tab2, text='About')
-
-    playlist = Entry(tab1, width=40)
-    playlist.place(rely=0.33, relx=0.5, x=0, y=0, anchor=CENTER)
-
-    btn1 = Button(tab1, text='Download Playlist', command=setplaylist)
-
-    btn1.place(rely=1.0, relx=1.0, x=0, y=0, anchor=SE)
-
-    btn2 = Button(tab1, text='Download 1 Song', command=setsong)
-
-    btn2.place(rely=1.0, relx=0, x=0, y=0, anchor=SW)
     
-    pb = ttk.Progressbar(tab1, orient='horizontal', mode='determinate', length=280)
-    pb.place(rely=0.6, relx=0.5, anchor=S)
-    lbl_Progress = Label(tab1, text="0/0 MB")
-    lbl_Progress.place(rely=0.57, relx=0.90, x=0, y=0, anchor=CENTER)
-
-    pb1 = ttk.Progressbar(tab1, orient='horizontal', mode='determinate', length=280)
-    pb1.place(rely=0.8, relx=0.5, anchor=S)
-
-    lbl_j = Label(tab2, text="Created for my beloved dory")
-    lbl_j.place(rely=0.5, relx=0.6, x=0, y=0, anchor=CENTER)
-
-    lbl_c = Label(tab2, text="©2023 Michael Schmid")
-    lbl_c.place(rely=1, relx=1, x=0, y=0, anchor=SE)
-
-    tab_control.pack(expand=1, fill='both')
-
-    photo = PhotoImage(file="dory.gif", format="gif -index 15", )
-    canvas = Canvas(tab2, width=85, height=100)
-    canvas.pack()
-
-    canvas.create_image(0, 0, anchor=NW, image=photo)
-    canvas.place(relx=0.0, rely=1.0, anchor=SW)
-
-    window.geometry('450x250')
-    window.resizable(False, False)
-    window.mainloop()
-    try:
-        window.attributes('-topmost', 1)
-        window.update()
-        window.attributes('-topmost', 0)
-        window.iconify()
-        window.deiconify()
-
-    except:
-        print("closed")
-
-
-dory2mp3()
--- test.py
+++ test.py
@@ -3,71 +3,101 @@ from moviepy.audio.io.AudioFileClip import AudioFileClip
 from tkinter import *
 from mutagen.easyid3 import EasyID3
 from pytube import YouTube,Playlist
+from pytube.cli import on_progress
+from proglog import ProgressBarLogger
 import os
+import string
 import threading
-import pytube
 
-amount_of_songs = 0
-downloaded_songs = 0
 
-# prints each video url, which is the same as iterating through playlist.video_urls
 
+fDownloadSize=0
+fDownloaded=0
+
+fConvertSize=0
+fConverted=0
 
 destination = os.path.join(os.path.expanduser("~"), "Music")
 
 
 def dory2mp3():
     def setplaylist():
-        print('test')
-        playlist_entered = Playlist(playlist.get())
-        for url in playlist_entered:
-            global amount_of_songs
-            amount_of_songs += 1
-            lbl_progress.configure(
-            text=str(downloaded_songs)+" von " + str(amount_of_songs))
-            urlstr = str(url)
-            print(urlstr)
-
-            lbl_progress.configure(
-                text=str(downloaded_songs)+" von " + str(amount_of_songs))
+        oList = Playlist(playlist.get())
+        for oContent in oList.videos:
             threading.Thread(target=downloadStream,
-                             args=(url, destination)).start()
+                             args=(oContent, destination, oList.length)).start()
 
             # prints address of each YouTube object in the playlist
     def setsong():
-        global amount_of_songs
-        amount_of_songs += 1
-        vid = playlist.get()
+        url = playlist.get()
+        oVideo=YouTube(url)
         threading.Thread(target=downloadStream,
-                         args=(vid, destination)).start()
+                         args=(oVideo, destination, 1)).start()
 
-    def downloadStream(sURL, destination):
-        oContent = YouTube(sURL)
-        sFileName=destination + "\\" + oContent.title + ".mp3"
-
-        #Get best quality audio-stream and convert it to mp3
-        oStream = oContent.streams.filter(mime_type='audio/mp4').order_by('abr').last()
+    def downloadStream(oContent, destination, iPlaylistLength):
+        global fDownloadSize
+        
+        if oContent.age_restricted:
+            print("Age restricted")
+            return
+        
+        try:
+            #Get best quality audio-stream and convert it to mp3
+            oStream = oContent.streams.get_audio_only()
+            
+        except:
+            print("Stream couldn't be accessed")
+            return
+        fDownloadSize+=round(oStream.filesize_mb,1)
+        oContent.register_on_progress_callback(my_progress_function)
+        sFileName=destination +"/"+ remove_invalid_chars(oContent.title) + ".mp3"
+        print("started Downloading Video:"+oContent.title )
+        
         oVideo=oStream.download(output_path=destination)
         oAudio = AudioFileClip(oVideo)
-        oAudio.write_audiofile(sFileName)
-
+        oAudio.write_audiofile(sFileName,logger=logger)
+        try:
+            #Get best quality audio-stream and convert it to mp3
+            oAudioMetadata = EasyID3(sFileName)
+            oAudioMetadata['title'] = oContent.title
+            oAudioMetadata['artist'] = oContent.author 
+            oAudioMetadata.save()
+        except:
+            print("Stream couldn't be accessed")
+            return
+        
         # Add metadata using mutagen
-        oAudioMetadata = EasyID3(sFileName)
-        oAudioMetadata['title'] = oContent.title
-        oAudioMetadata['artist'] = oContent.author 
-        oAudioMetadata.save()
+
 
 
         oAudio.close()
-
         #remove uncecessary mp4
         os.remove(oVideo)
+    def remove_invalid_chars(filename):
+        valid_chars = "-_.() %s%s" % (string.ascii_letters, string.digits)
+        return ''.join(c for c in filename if c in valid_chars)
+    def my_progress_function(stream, chunk, bytes_remaining):
+        global fDownloaded
+        global fDownloadSize
+        
+        if bytes_remaining > 9437184:
+            fDownloaded+=9
+        else:
+            fDownloaded+=round(len(chunk)/1048576,1)
 
-        #progress calculation - still needs rework
-        global downloaded_songs
-        downloaded_songs += 1
-        lbl_progress.configure(
-            text=str(downloaded_songs)+" von " + str(amount_of_songs))
+        #progress = round(bytes_downloaded / total_size * 100)
+        pb['value'] = 100*fDownloaded/fDownloadSize
+        lbl_Progress['text']=fDownloaded,"/",fDownloadSize," MB"
+    class MyBarLogger (ProgressBarLogger):
+        def bars_callback (self, bar, attr, value,old_value=0):
+            global fConvertSize
+            global fConverted
+            if old_value is None:
+                fConvertSize+=self.bars [bar] ['total']
+                return 
+            fConverted+=value-old_value
+            pb1['value'] = 100*fConverted/fConvertSize
+    logger = MyBarLogger ()
     window = Tk()
     window.title("Dory2mp3")
     window.iconbitmap("dory.ico")
@@ -83,10 +113,7 @@ def dory2mp3():
     tab_control.add(tab2, text='About')
 
     playlist = Entry(tab1, width=40)
-    playlist.place(rely=0.5, relx=0.5, x=0, y=0, anchor=CENTER)
-    lbl_progress = Label(tab1, text=str(
-        downloaded_songs) + "von" + str(amount_of_songs))
-    lbl_progress.place(rely=0, relx=1, x=0, y=0, anchor=NE)
+    playlist.place(rely=0.33, relx=0.5, x=0, y=0, anchor=CENTER)
 
     btn1 = Button(tab1, text='Download Playlist', command=setplaylist)
 
@@ -95,6 +122,14 @@ def dory2mp3():
     btn2 = Button(tab1, text='Download 1 Song', command=setsong)
 
     btn2.place(rely=1.0, relx=0, x=0, y=0, anchor=SW)
+    
+    pb = ttk.Progressbar(tab1, orient='horizontal', mode='determinate', length=280)
+    pb.place(rely=0.6, relx=0.5, anchor=S)
+    lbl_Progress = Label(tab1, text="0/0 MB")
+    lbl_Progress.place(rely=0.57, relx=0.90, x=0, y=0, anchor=CENTER)
+
+    pb1 = ttk.Progressbar(tab1, orient='horizontal', mode='determinate', length=280)
+    pb1.place(rely=0.8, relx=0.5, anchor=S)
 
     lbl_j = Label(tab2, text="Created for my beloved dory")
     lbl_j.place(rely=0.5, relx=0.6, x=0, y=0, anchor=CENTER)
@@ -111,7 +146,7 @@ def dory2mp3():
     canvas.create_image(0, 0, anchor=NW, image=photo)
     canvas.place(relx=0.0, rely=1.0, anchor=SW)
 
-    window.geometry('450x160')
+    window.geometry('450x250')
     window.resizable(False, False)
     window.mainloop()
     try:
